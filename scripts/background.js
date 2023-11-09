43d644bd3a8ea602c1c2fd6cb496eb9b2cec018f
Matheus Dias <matheusdias.front96@gmail.com>
2023-11-09 16:50:02 -0300

    Implementação das posições da imagem no BG

--- scripts/background.js
+++ scripts/background.js
@@ -1,8 +1,12 @@
 const buttonPrev = document.querySelector('[data-js="btn"]');
 const buttonNext = document.querySelector('[data-js="btn2"]');
 const backgroundImage = document.querySelector('[data-js="background-image"]');
-const title = document.querySelector('h1')
-const paragraph = document.querySelector('p')
+
+const colorElements = [
+  document.querySelector('[data-js="active-color1"]'),
+  document.querySelector('[data-js="active-color2"]'),
+  document.querySelector('[data-js="active-color3"]'),
+];
 
 let counter = 1;
 
@@ -10,14 +14,24 @@ const addBackgroundClass = () => {
   backgroundImage.className = `background${counter}`;
 };
 
+const carouselControlColor = () => {
+  colorElements.forEach((color, index) => {
+    const isActive = counter === index + 1;
+    color.classList.toggle('active-color', isActive);
+    color.classList.toggle('off-color', !isActive);
+  });
+};
+
 const prevButtonClass = () => {
   counter = counter === 1 ? 3 : counter - 1;
   updateTextAndBackground();
+  carouselControlColor();
 };
 
 const nextButtonClass = () => {
   counter = counter === 3 ? 1 : counter + 1;
   updateTextAndBackground();
+  carouselControlColor();
 };
 
 buttonNext.addEventListener('click', nextButtonClass);
--- scripts/textEfect.js
+++ scripts/textEfect.js
@@ -1,3 +1,22 @@
+const title = document.querySelector('h1');
+const paragraph = document.querySelector('p');
+const span = document.querySelector('span');
+
+const textBG1 = {
+  h1: 'DESIGN',
+  p: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.',
+}
+
+const textBG2 = {
+  h1: 'MOTION',
+  p: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum.',
+}
+
+const textBG3 = {
+  h1: 'ENGINE',
+  p: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum..',
+}
+
 const textTransitionEffect = () => {
   title.style.transition = 'opacity 0.8s ease';
   paragraph.style.transition = 'opacity 0.8s ease';
@@ -7,12 +26,14 @@ const insertTextInBG = ({ h1, p }) => {
   textTransitionEffect();
   title.style.opacity = '0';
   paragraph.style.opacity = '0';
+  span.style.opacity = '0'
 
   setTimeout(() => {
     title.textContent = h1;
     paragraph.textContent = p;
     title.style.opacity = '1';
     paragraph.style.opacity = '1';
+    span.style.opacity = '1'
   }, 555);
 };
 
