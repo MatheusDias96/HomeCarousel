64dbe13039b45d25128cdc5dcde925a249c9319d
Rafael Dias <rafael.ddsociety@gmail.com>
2023-11-11 12:49:35 -0300

    fix imports, css, e js

--- scripts/background.js
+++ scripts/background.js
@@ -1,38 +1,48 @@
-const buttonPrev = document.querySelector('[data-js="btn"]');
-const buttonNext = document.querySelector('[data-js="btn2"]');
-const backgroundImage = document.querySelector('[data-js="background-image"]');
-
+const buttonPrev = document.querySelector('[data-js="btn"]')
+const buttonNext = document.querySelector('[data-js="btn2"]')
+const backgroundImage = document.querySelector('[data-js="background-image"]')
 const colorElements = [
   document.querySelector('[data-js="active-color1"]'),
   document.querySelector('[data-js="active-color2"]'),
   document.querySelector('[data-js="active-color3"]'),
-];
-
-let counter = 1;
+]
+let counter = 1
+let isTransitioning = false
 
 const addBackgroundClass = () => {
-  backgroundImage.className = `background${counter}`;
-};
+  backgroundImage.className = `background${counter}`
+}
 
 const carouselControlColor = () => {
   colorElements.forEach((color, index) => {
-    const isActive = counter === index + 1;
-    color.classList.toggle('active-color', isActive);
-    color.classList.toggle('off-color', !isActive);
-  });
-};
+    const isActive = counter === index + 1
+    color.classList.toggle('active-color', isActive)
+    color.classList.toggle('off-color', !isActive)
+  })
+}
 
 const prevButtonClass = () => {
-  counter = counter === 1 ? 3 : counter - 1;
-  updateTextAndBackground();
-  carouselControlColor();
-};
+  if (isTransitioning) return
+  counter = counter === 1 ? 3 : counter - 1
+  updateTextandParagraph()
+  carouselControlColor()
+}
 
 const nextButtonClass = () => {
-  counter = counter === 3 ? 1 : counter + 1;
-  updateTextAndBackground();
-  carouselControlColor();
-};
+  if (isTransitioning) return
+  counter = counter === 3 ? 1 : counter + 1
+  updateTextandParagraph()
+  carouselControlColor()
+}
 
-buttonNext.addEventListener('click', nextButtonClass);
-buttonPrev.addEventListener('click', prevButtonClass);
+const updateTextAndBackground = () => {
+  isTransitioning = true
+  addBackgroundClass()
+  carouselControlColor()
+  setTimeout(() => {
+    isTransitioning = false
+  }, 1000) // Tempo de transição em milissegundos, ajuste conforme necessário
+}
+
+buttonNext.addEventListener('click', nextButtonClass)
+buttonPrev.addEventListener('click', prevButtonClass)
